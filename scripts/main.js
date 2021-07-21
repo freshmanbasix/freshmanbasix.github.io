5332334d2bd3dc3dcd5893420a4d3ac1cf1b6b0c
hjalle <david@hjallesoft.com>
2021-07-21 14:38:45 +0200

    article updates with fetch()

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,24 @@
-let currentActiveWiki = document.getElementById("welcome");
-currentActiveWiki.classList.add("active-wiki-entry");
-
 let currentActiveToc = document.getElementById("welcome-toc");
 currentActiveToc.classList.add("active-toc");
 
 function setActive(entryName) {
-  currentActiveWiki.classList.remove("active-wiki-entry");
   currentActiveToc.classList.remove("active-toc");
-
-  let newActiveWiki = document.getElementById(entryName);
-  newActiveWiki.classList.add("active-wiki-entry");
-  currentActiveWiki = newActiveWiki;
 
   let newActiveToc = document.getElementById(entryName + "-toc");
   newActiveToc.classList.add("active-toc");
   currentActiveToc = newActiveToc;
 
-  console.log(entryName);
   console.log(entryName + "-toc");
 }
+
+function newContent(fileName) {
+  let wikiElement = document.getElementById("wiki-entry-container");
+
+  fetch(fileName)
+  .then(response => response.text())
+  .then((data) => {
+    wikiElement.innerHTML = data
+  })
+}
+
+newContent('articles/welcome.txt')
