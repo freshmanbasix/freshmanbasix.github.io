db6030e6d874f80852f833cee0c0d82241a6a2fe
hjalle <david@hjallesoft.com>
2022-01-21 13:30:02 +0100

    publish radio, email and electronics articles

--- scripts/wiki-column.js
+++ scripts/wiki-column.js
@@ -12,10 +12,13 @@
             new Route('encryption', 'encryption.html'),
             new Route('servers', 'servers.html'),
             new Route('web', 'web.html'),
+            new Route('email', 'email.html'),
             new Route('terminals', 'terminals.html'),
             new Route('linux', 'linux.html'),
             new Route('version-control', 'version-control.html'),
             new Route('editors', 'editors.html'),
+            new Route('electronics', 'electronics.html'),
+            new Route('radio', 'radio.html'),
             new Route('probability-statistics', 'probability-statistics.html'),
             new Route('contribute', 'contribute.html')
         ], 'wiki-column');
