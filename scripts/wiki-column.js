553656d667fe23de400e7ec911e565ef138199a9
hjalle <david@hjallesoft.com>
2022-03-15 11:22:46 +0100

    sorted articles alphabetically

--- scripts/wiki-column.js
+++ scripts/wiki-column.js
@@ -9,21 +9,21 @@
     function init() {
         var router = new Router([
             new Route('welcome', 'welcome.html', true),
+            new Route('bash', 'bash.html'),
+            new Route('bike-mechanics', 'bike-mechanics.html'),
+            new Route('calculus', 'calculus.html'),
+            new Route('editors', 'editors.html'),
+            new Route('electronics', 'electronics.html'),
+            new Route('email', 'email.html'),
             new Route('encryption', 'encryption.html'),
+            new Route('git', 'git.html'),
+            new Route('linux', 'linux.html'),
+            new Route('physics', 'physics.html'),
+            new Route('probability-statistics', 'probability-statistics.html'),
+            new Route('programming', 'programming.html'),
+            new Route('radio', 'radio.html'),
             new Route('servers', 'servers.html'),
             new Route('web', 'web.html'),
-            new Route('email', 'email.html'),
-            new Route('bash', 'bash.html'),
-            new Route('linux', 'linux.html'),
-            new Route('git', 'git.html'),
-            new Route('editors', 'editors.html'),
-            new Route('programming', 'programming.html'),
-            new Route('physics', 'physics.html'),
-            new Route('electronics', 'electronics.html'),
-            new Route('radio', 'radio.html'),
-            new Route('calculus', 'calculus.html'),
-            new Route('probability-statistics', 'probability-statistics.html'),
-            new Route('bike-mechanics', 'bike-mechanics.html'),
             new Route('contribute', 'contribute.html')
         ], 'wiki-column');
     }
